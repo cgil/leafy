a9d3932f069a5a60024706a175d8081d331dc1bf
Carlos Gil <cgil1210@gmail.com>
2013-08-16 15:33:01 -0400

    remved apps route file

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,18 @@
 exports.index = function(req, res){
 	res.sendfile('/index.html', {'root': 'public/'});
 };
+
+exports.apps = function(req, res){
+	var appName = req.params.appName;
+
+	var callback = req.query.callback;
+	var args = req.query.args;
+
+	res.header('Content-Type', 'text/html');
+	res.header('Charset', 'utf-8');
+	var response = '{"app": "'+ appName +'", "args": "'+ args +'"}';
+	if(typeof callback !== 'undefined') {
+		response = callback + '(' + response + ');'; 
+	}  
+	res.send(response);
+};
