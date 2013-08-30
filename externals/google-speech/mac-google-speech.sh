28cda08c142080eee9942cd22707a3d430b31e57
Carlos Gil <cgil1210@gmail.com>
2013-08-29 23:22:58 -0400

    add other speech options and mac-google-speech

---
+++ externals/google-speech/mac-google-speech.sh
@@ -0,0 +1,13 @@
+ #!/bin/sh
+ echo "1 SoX Sound Exchange - Convert WAV to FLAC with 16000" 
+ sox ./sound/message.wav ./sound/message.flac rate 16k
+ echo "2 Submit to Google Voice Recognition"
+ wget -q -U "Mozilla/5.0" --post-file ./sound/message.flac --header="Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" > message.ret
+ echo "3 SED Extract recognized text" 
+ cat message.ret | sed 's/.*utterance":"//' | sed 's/","confidence.*//' > message.txt
+ echo "4 Remove Temporary Files"
+ #rm ./sound/message.flac
+ rm message.ret
+ echo "5 Show Text "
+ cat message.txt
+
